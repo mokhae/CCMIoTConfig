7edac2931a2625ea154649f3c382aa205ea0f0d9
mokhae <mokhae70@gmail.com>
2025-08-13 15:22:23 +0900

    MH : Update MB Server Registry Name

--- iotconfig/iotconfig.go
+++ iotconfig/iotconfig.go
@@ -240,23 +240,23 @@ func (s MB_SERVER_REGISTRY) String() string {
 }
 
 type MC3E_CHANNEL struct {
-	COMMAND         MC3E_COMMAND       `json:"COMMAND"`
-	DEVICE_CODE     MC_DEVICE_CODE     `json:"DEVICE_CODE"`
-	OFFSET          int64              `json:"OFFSET"`
-	NUM_POINT       int64              `json:"NUM_POINT"`
-	MBSERVER_START  int                `json:"MBSERVER_START"`
-	MBSERVER_MEMORY MB_SERVER_REGISTRY `json:"MBSERVER_MEMORY"`
-	ENABLED         bool               `json:"ENABLED"`
+	COMMAND           MC3E_COMMAND       `json:"COMMAND"`
+	DEVICE_CODE       MC_DEVICE_CODE     `json:"DEVICE_CODE"`
+	OFFSET            int64              `json:"OFFSET"`
+	NUM_POINT         int64              `json:"NUM_POINT"`
+	MBSERVER_START    int                `json:"MBSERVER_START"`
+	MBSERVER_REGISTRY MB_SERVER_REGISTRY `json:"MBSERVER_REGISTRY"`
+	ENABLED           bool               `json:"ENABLED"`
 }
 
 type MB_CHANNEL struct {
-	SLAVE_ID        byte               `json:"SLAVE_ID"`
-	COMMAND         MB_COMMAND         `json:"COMMAND"`
-	OFFSET          int                `json:"OFFSET"`
-	NUM_POINT       int                `json:"NUM_POINT"`
-	MBSERVER_START  int                `json:"MBSERVER_START"`
-	MBSERVER_MEMORY MB_SERVER_REGISTRY `json:"MBSERVER_MEMORY"`
-	ENABLED         bool               `json:"ENABLED"`
+	SLAVE_ID          byte               `json:"SLAVE_ID"`
+	COMMAND           MB_COMMAND         `json:"COMMAND"`
+	OFFSET            int                `json:"OFFSET"`
+	NUM_POINT         int                `json:"NUM_POINT"`
+	MBSERVER_START    int                `json:"MBSERVER_START"`
+	MBSERVER_REGISTRY MB_SERVER_REGISTRY `json:"MBSERVER_REGISTRY"`
+	ENABLED           bool               `json:"ENABLED"`
 }
 
 func LoadJSONFile[T any](filePath string) ([]T, error) {
